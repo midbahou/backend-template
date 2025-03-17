5fd52b85f7f971861bc00c7b2f0a9ab7d8d1cc45
midbahou <midbahou@gmail.com>
2025-03-17 11:39:42 -0400

    routes and public folder created

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import express from 'express';
+import express from 'express'; // to set up a full-stack application
+import morgan from 'morgan';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+console.log(process.env.MONGODB_URI);
+
+
 const PORT = process.env.PORT || 4000;
 const app = express()
 
@@ -8,7 +17,13 @@ app.set('view engine', 'pug')
 
 // Middleware: functions executed in the middle between the request and the response
 app.use(express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'));
+app.use(helmet()); // add more security to your app
+app.use(cors()); // if we have another app running it will be blocked by cors
 
+// Routes
 app.get('/', (req, res) => {
     res.render('index')
 })
